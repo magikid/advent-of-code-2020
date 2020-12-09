137a2c24efc810cee8964a5cd4e383dbf16ce641
Chris W Jones <Christopher.Jones@sourceability.com>
2020-12-09 17:21:14 -0500

    All d9p2 tests passing

--- day9_test.go
+++ day9_test.go
@@ -36,9 +36,35 @@ func TestExamplePart1(t *testing.T) {
 }
 
 func TestExamplePart2(t *testing.T) {
-	exampleInput1 := []string{"1", "3", "5", "7", "9"}
+	exampleInput2 := []string{
+		"35",
+		"20",
+		"15",
+		"25",
+		"47",
+		"40",
+		"62",
+		"55",
+		"65",
+		"95",
+		"102",
+		"117",
+		"150",
+		"182",
+		"127",
+		"219",
+		"299",
+		"277",
+		"309",
+		"576",
+	}
+	cipher2 := makeXmasCipherBase(exampleInput2, 5)
+	assert.True(t, cipher2.Broken(), "cipher: %v", cipher2)
+	assert.Equal(t, 127, cipher2.weaknessTarget)
+	assert.Equal(t, 62, cipher2.FindWeakness())
+
+	exampleInput1 := []string{"1", "3", "5", "7", "9", "15"}
 	cipher := makeXmasCipherBase(exampleInput1, 5)
-	cipher.Add(100)
 	assert.True(t, cipher.Broken())
-	assert.Equal(t, 0, cipher.FindWeakness())
+	assert.Equal(t, 10, cipher.FindWeakness())
 }
--- xmas_cipher.go
+++ xmas_cipher.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"log"
+	"sort"
 	"strconv"
 )
 
 type xmasCipher struct {
+	rawInput       []int
 	preambleLength int
 	preamble       []int
 	contents       map[int]bool
@@ -14,7 +16,7 @@ type xmasCipher struct {
 }
 
 func (cipher xmasCipher) String() string {
-	return fmt.Sprintf("target: %v, preamble: %v", cipher.weaknessTarget, cipher.preamble)
+	return fmt.Sprintf("target: %v, preambleLength: %v, preamble: %v, contents: %v, input: %v", cipher.weaknessTarget, cipher.preambleLength, cipher.preamble, cipher.contents, cipher.rawInput)
 }
 
 func (cipher *xmasCipher) UpdatePreamble(nextNumber int) {
@@ -29,6 +31,8 @@ func (cipher *xmasCipher) UpdatePreamble(nextNumber int) {
 }
 
 func (cipher *xmasCipher) Add(nextNumber int) {
+	cipher.contents = make(map[int]bool)
+
 	for i := 0; i < cipher.preambleLength; i++ {
 		tempSum := nextNumber - cipher.preamble[i]
 		_, ok := cipher.contents[tempSum]
@@ -56,25 +60,60 @@ func (cipher *xmasCipher) FindWeakness() int {
 		return 0
 	}
 
+	targetIndex := 0
+	for i, val := range cipher.rawInput {
+		if val == cipher.weaknessTarget {
+			targetIndex = i
+			break
+		}
+	}
+
+	for i := 0; i <= targetIndex; i++ {
+		for j := targetIndex; j > 0; j-- {
+			testRange := cipher.rawInput[i:j]
+			if sum(testRange) == cipher.weaknessTarget {
+				sort.Ints(testRange)
+				return testRange[0] + testRange[len(testRange)-1]
+			}
+		}
+	}
+
 	return 0
 }
 
-func makeXmasCipherBase(input []string, preambleLength int) xmasCipher {
-	preamble := make([]int, preambleLength)
-	contents := make(map[int]bool)
-	cipher := xmasCipher{contents: contents, preambleLength: preambleLength, preamble: preamble}
+func sum(numbers []int) int {
+	result := 0
+	for _, number := range numbers {
+		result += number
+	}
 
-	if len(input) < cipher.preambleLength {
-		log.Printf("Preable too small! %v", input)
+	return result
+}
+
+func makeXmasCipherBase(input []string, preambleLength int) xmasCipher {
+	contents := make(map[int]bool)
+	rawInput := make([]int, len(input))
+	cipher := xmasCipher{contents: contents, preambleLength: preambleLength}
+
+	for i, value := range input {
+		rawInput[i] = makeNumber(value)
+	}
+	cipher.rawInput = rawInput
+
+	preamble := make([]int, cipher.preambleLength)
+	cipher.preamble = preamble
+
+	if len(cipher.rawInput) < cipher.preambleLength {
+		log.Printf("Preable too small! %v", cipher.rawInput)
 		return cipher
 	}
 
 	for i := 0; i < cipher.preambleLength; i++ {
-		cipher.UpdatePreamble(makeNumber(input[i]))
+		cipher.UpdatePreamble(cipher.rawInput[i])
 	}
 
-	for i := 25; i < len(input); i++ {
-		cipher.Add(makeNumber(input[i]))
+	for i := cipher.preambleLength; i < len(cipher.rawInput); i++ {
+		cipher.Add(cipher.rawInput[i])
 		if cipher.Broken() {
 			return cipher
 		}
